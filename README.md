e1424a462fbf7ec663dbd4c5e1ed8dac89239157
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-08 13:56:59 +0100

    auth layout

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ Rutas Principales
 LazyLoad
 
 Multiples estilos en la misma SPA
+
+Angular Material
+
+Interfaces y tipado
+
+Pipes personalizados
+
+Variables de entorno
+
+Autocomplete de AngularMaterial
+
+Peticiones HTTP
+
+JSON-Server
+
+Prime Flex
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
 import { SharedModule } from './shared/shared.module';
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, MaterialModule],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
