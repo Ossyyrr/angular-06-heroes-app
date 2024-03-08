98aae8b4e7eefc37dd40ac4d3995f8c576ba7856
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-08 14:19:24 +0100

    sidenav

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { MaterialModule } from '../material/material.module';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
@@ -8,18 +9,14 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 
-
 @NgModule({
   declarations: [
     HeroPageComponent,
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
   ],
-  imports: [
-    CommonModule,
-    HeroesRoutingModule
-  ]
+  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
-export class HeroesModule { }
+export class HeroesModule {}
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styleUrl: './layout-page.component.css'
+  styleUrl: './layout-page.component.css',
 })
 export class LayoutPageComponent {
-
+  public sidebarItems = [
+    { label: 'Listado', icon: 'label', link: './list' },
+    { label: 'Añadir', icon: 'add', link: './new-hero' },
+    { label: 'Buscar', icon: 'search', link: './search' },
+  ];
 }
